c388b85dae6f675b230a2c2e8c76d25f98caabc4
Jessuh <58728143+Jessuhh@users.noreply.github.com>
2022-03-29 17:18:00 +0200

    Initial commit

---
+++ src/main/java/dev/jessuh/velocitysavermod/mixin/VelocitySaverMixin.java
@@ -0,0 +1,16 @@
+package dev.jessuh.velocitysavermod.mixin;
+
+import dev.jessuh.velocitysavermod.VelocitySaverMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class VelocitySaverMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+//		VelocitySaverMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
